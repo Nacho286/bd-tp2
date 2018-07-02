458ad05c78a299bd98bc50417ae304eca83a2d36
ignacio roberto rodriguez <ignacio.igrodriguez@mercadolibre.com>
2018-07-01 21:08:04 -0300

    Cambios

--- create-tables.py
+++ create-tables.py
@@ -8,14 +8,14 @@ databases = r.db_list().run()
 if 'tp2' in databases:
     r.db_drop('tp2').run()
 
-print 'creando bd'
+print('creando bd')
 r.db_create('tp2').run()
 
 # Crear tablas
 tables = r.table_list().run()
 for table in tablelist:
     if table not in tables:
-        print 'creando tabla: ' + table
+        print('creando tabla: ' + table)
         r.table_create(table).run()
 
 r.table('clientes').reconfigure(shards=3,replicas=1).run()
