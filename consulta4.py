baa54e23739f65285231a54d22e22c538e92c011
Lautaro Alvarez <lautarolalvarez@gmail.com>
2018-07-05 22:44:02 -0300

    Agrego un posible reduce de la consulta 4

--- consulta4.py
+++ consulta4.py
@@ -7,5 +7,12 @@ r.connect('localhost', 28015, db='tp2').repl()
 
 locacion = int(sys.argv[1])
 
-print(r.db('tp2').table('parques').filter({'idLocacion':locacion})["atracciones"].map(lambda x:
-  x.pluck("nombre",{"visitas":"precio"})).run())
+rows = r.db('tp2').table('parques').filter({'idLocacion':locacion})["atracciones"].map(lambda x:
+  x.pluck("nombre",{"visitas":"precio"})).run()
+
+for parque in rows:
+    for atraccion in parque:
+        total = 0
+        for visita in atraccion['visitas']:
+            total += visita['precio']
+        print(atraccion['nombre'] + " $" + str(total))
